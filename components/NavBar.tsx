135499c25b48ffd6b48cc02b5fa28e85f60c6bbc
Stephen Farmer <sfarmer@majorcode.com>
2022-11-18 13:20:28 -0500

    Move nav logo for better alignment

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,8 +2,10 @@ export default function NavBar() {
 	return (
 		<div class="navbar bg-base-100">
 			<div class="flex-1">
-				<img src="/img/heart.svg" style="width: 36px; height: 36px;" />
-				<a href="/" class="btn btn-ghost normal-case text-xl">Nanfarm</a>
+				<a href="/" class="btn btn-ghost normal-case text-xl">
+					<img src="/img/heart.svg" style="width: 36px; height: 36px;" />
+					<span class="pl-2">Nanfarm</span>
+				</a>
 			</div>
 			<div class="flex-none">
 				<ul class="menu menu-horizontal p-0">
