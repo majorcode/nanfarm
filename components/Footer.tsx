e79dfa8aff90c38c73d9356a2ef39fa91962d7be
Stephen Farmer <sfarmer@majorcode.com>
2022-11-18 15:04:20 -0500

    Fix footer wrapping

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,11 @@
 export default function Footer() {
 	return (
-		<footer class="footer items-center p-4 bg-neutral text-neutral-content">
-			<div class="items-center grid-flow-col">
+		<footer class="grid grid-cols-2 p-4 bg-neutral text-neutral-content">
+			<div class="flex items-center whitespace-nowrap">
 				<img src="/img/heart.svg" style="width: 36px; height: 36px;" />
 				<p>Copyright © 2022 - Nancy Farmer</p>
 			</div>
-			<div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-				{/* Facebook */}
+			<div class="flex justify-end items-center">
 				<a href="https://www.facebook.com/nanfarm">
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
 				</a>
