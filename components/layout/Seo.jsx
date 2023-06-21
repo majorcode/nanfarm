ba004adc1d90aeaa510a5daf775a9b666c06fdc7
Stephen Farmer <sfarmer@majorcode.com>
2023-06-21 16:06:36 -0400

    MOve Seo into JSX to stop incorrect type error for the link tag color attribute

---
+++ components/layout/Seo.jsx
@@ -0,0 +1,27 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Seo({ ...customMeta }) {
+	const meta = {
+		title: "Untitled",
+		description: "no description",
+		...customMeta,
+	};
+
+	// https://medium.com/swlh/are-you-using-svg-favicons-yet-a-guide-for-modern-browsers-836a6aace3df
+
+	// There are suggestions to do icons better, but Safari still seems to want a mask-icon.
+	// https://dev.to/masakudamatsu/favicon-nightmare-how-to-maintain-sanity-3al7
+
+	return (
+		<Head>
+			<title>{meta.title}</title>
+			<meta content={meta.description} name="description" />
+			<meta name="theme-color" content="#ffffff"></meta>
+			<link rel="icon" href="favicon.svg" type="image/svg" />
+			<link rel="mask-icon" href="icon-mask.svg" color="#b3404a" />
+			<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+			<link rel="manifest" href="manifest.json" />
+			<link rel="stylesheet" href="/css/compiled.css"></link>
+		</Head>
+	);
+}
--- components/layout/container.tsx
+++ components/layout/container.tsx
@@ -1,5 +1,5 @@
 import { ComponentChildren } from "preact";
-import { Head } from "$fresh/runtime.ts";
+import Seo from "./Seo.jsx";
 import NavBar from "/components/NavBar.tsx";
 import Footer from "/components/Footer.tsx";
 
@@ -20,31 +20,5 @@ export const Container = ({ children, ...customMeta }: Props) => {
 				<Footer />
 			</div>
 		</>
-	);
-};
-
-const Seo = ({ ...customMeta }) => {
-	const meta = {
-		title: "Untitled",
-		description: "no description",
-		...customMeta,
-	};
-
-	// https://medium.com/swlh/are-you-using-svg-favicons-yet-a-guide-for-modern-browsers-836a6aace3df
-
-	// There are suggestions to do icons better, but Safari still seems to want a mask-icon.
-	// https://dev.to/masakudamatsu/favicon-nightmare-how-to-maintain-sanity-3al7
-
-	return (
-		<Head>
-			<title>{meta.title}</title>
-			<meta content={meta.description} name="description" />
-			<meta name="theme-color" content="#ffffff"></meta>
-			<link rel="icon" href="favicon.svg" type="image/svg" />
-			<link rel="mask-icon" href="icon-mask.svg" color="#b3404a" />
-			<link rel="apple-touch-icon" href="apple-touch-icon.png" />
-			<link rel="manifest" href="manifest.json" />
-			<link rel="stylesheet" href="/css/compiled.css"></link>
-		</Head>
 	);
 };
