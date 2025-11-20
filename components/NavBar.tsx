0b2f66ddbd78d59244289ed2a8d52e3e2933a826
Stephen Farmer <sfarmer@majorcode.com>
2025-11-20 18:58:38 -0500

    Update NavBar button styles for improved hover effect

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,7 @@ export default function NavBar() {
 	return (
 		<div class="navbar bg-base-300 mb-8">
 			<div class="flex-1">
-				<a href="/" class="btn btn-ghost normal-case text-xl">
+				<a href="/" class="btn btn-ghost text-xl hover:bg-base-content/8 border-0">
 					<img src="/img/heart.svg" style="width: 36px; height: 36px;" />
 					<span class="pl-2">Nanfarm</span>
 				</a>
