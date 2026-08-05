eb92ff30fc2f95703e29b78418c4a6af0e8c646e
Stephen Farmer <sfarmer@majorcode.com>
2026-08-04 22:09:25 -0400

    fix: correct language attribute in work-louder.html from 'em' to 'en'

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,16 @@
 import { define } from "../utils.ts";
 
 export default define.page(function App({ Component }) {
-  return (
-    <html data-theme="valentine">
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>nanfarm</title>
-      </head>
-      <body>
-        <Component />
-      </body>
-    </html>
-  );
+	return (
+		<html data-theme="valentine" lang="en">
+			<head>
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>nanfarm</title>
+			</head>
+			<body>
+				<Component />
+			</body>
+		</html>
+	);
 });
