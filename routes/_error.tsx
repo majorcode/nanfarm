c68eace66799db455e0498ef6039acb02ee6a4f6
Stephen Farmer <sfarmer@majorcode.com>
2026-08-10 11:48:25 -0400

    fix: update error handling to provide specific messages for 404 errors

--- routes/_error.tsx
+++ routes/_error.tsx
@@ -5,11 +5,12 @@ export default define.page(({ error, state, url}) => {
 
 	if (error instanceof HttpError) {
 		const status = error.status; // HTTP status code
-		state.title = `${status} ${error.message}`;
-		state.description = `An error occurred: ${error.message}`;
 
 		// Render a 404 not found page
 		if (status === 404) {
+			state.title = `Not Found`;
+			state.description = `The page you were looking for does not exist.`;
+
 			return (
 				<section class="prose">
 					<h1>Page Not Found</h1>
