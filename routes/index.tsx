8c0ac78aa761ccac83cc3ade88203717d5ef5f5a
Stephen Farmer <sfarmer@majorcode.com>
2026-08-10 18:19:22 -0400

    remove comment

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,7 @@ import { define } from "@/utils.ts";
 
 export default define.page(({ state }) => {
 	state.title = "Home";
-	state.description = "Welcome to Nancy Farmer's personal web site."; // optional
+	state.description = "Welcome to Nancy Farmer's personal web site.";
 
 	return (
 		<img src="/img/photo.png" class="my-4 rounded-lg" alt="Nancy Farmer" />
